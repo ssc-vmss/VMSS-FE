08c74e99c688575afa4a038b2c1fc57fd7a1b76b
Huangshiyi <huangsy53@chinaunicom.cn>
2018-09-05 15:57:09 +0800

    修改登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,34 +1,78 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-      <h3 class="title">车辆调度管理系统</h3>
-      <el-form-item prop="username">
-        <span class="svg-container svg-container_login">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input v-model="loginForm.username" name="username" type="text" auto-complete="on" placeholder="username" />
-      </el-form-item>
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :type="pwdType"
-          v-model="loginForm.password"
-          name="password"
-          auto-complete="on"
-          placeholder="password"
-          @keyup.enter.native="handleLogin" />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon icon-class="eye" />
-        </span>
-      </el-form-item>
-      <el-form-item>
-        <el-button :loading="loading" type="primary" style="width:100%;" @click.native.prevent="handleLogin">
-          登录
-        </el-button>
-      </el-form-item>
-    </el-form>
+    <!-- <vue-particles class="login-particle" color="#47CD88" :particleOpacity='0.7' :particleNumber="200" shapeType="circlr" :particleSize="4" linesColor="#47CD88" :lineWidth="1" :lineLinked="true" :lineOpacity="0.4" :lineDistance="150" :moveSpeed="3" :hoverEffect="true" hoverMode="grab" :clickEffect="true" clickMode="push"></vue-particles> -->
+    <div class="car-container">
+      <div class="car">
+        <div class="body">
+          <div class="mirror-wrap">
+            <div class="mirror-inner">
+              <div class="mirror">
+                <div class="shine"/>
+              </div>
+            </div>
+          </div>
+          <div class="middle">
+            <div class="top">
+              <div class="line"/>
+            </div>
+            <div class="bottom">
+              <div class="lights">
+                <div class="line"/>
+              </div>
+            </div>
+          </div>
+          <div class="bumper">
+            <div class="top"/>
+            <!-- <div class="middle" data-numb="&#2348;&#2366; &#2415;&#2411; &#2330; &#2415;&#2411;&#2415;&#2411;"></div> -->
+            <div class="bottom"/>
+          </div>
+        </div>
+        <div class="tyres">
+          <div class="tyre back"/>
+          <div class="tyre front"/>
+        </div>
+      </div>
+      <div class="road-wrap">
+        <div class="road">
+          <div class="lane-wrap">
+            <div class="lane">
+              <div/>
+              <div/>
+              <div/>
+              <div/>
+              <div/>
+              <div/>
+              <div/>
+              <div/>
+              <div/>
+              <div/>
+              <div/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="inner-container">
+      <div class="box">
+        <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+          <h1>车辆调度管理系统</h1>
+          <el-form-item prop="username">
+            <input v-model="loginForm.username" class="login-input" name="username" type="text" auto-complete="on" placeholder="用户名" >
+          </el-form-item>
+          <el-form-item prop="password">
+            <input :type="pwdType" v-model="loginForm.password" class="login-input" name="password" auto-complete="on" placeholder="密码" @keyup.enter.native="handleLogin" >
+          </el-form-item>
+          <el-form-item>
+            <el-button :loading="loading" type="primary" @click.native.prevent="handleLogin">
+              登录
+            </el-button>
+            <p>忘记密码?
+              <span>找回密码</span>
+            </p>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -56,6 +100,8 @@ export default {
       loginForm: {
         username: 'admin',
         password: 'admin'
+        // username: '',
+        // password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
@@ -66,13 +112,6 @@ export default {
     }
   },
   methods: {
-    showPwd() {
-      if (this.pwdType === 'password') {
-        this.pwdType = ''
-      } else {
-        this.pwdType = 'password'
-      }
-    },
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
@@ -94,92 +133,535 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-$bg:#2d3a4b;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$light_gray: #eee;
 
 /* reset element-ui css */
+.el-form-item {
+  text-align: center;
+}
 .login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      &:-webkit-autofill {
-        -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: #fff !important;
-      }
+  text-align: center;
+  height: 100%;
+  background-color: #2d3a4b;
+  .login-input {
+    text-align: center;
+    width: 330px;
+    margin: 15px auto;
+    padding: 15px;
+    background: rgba(0, 0, 0, 0.2);
+    color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    letter-spacing: 3px;
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+      -webkit-text-fill-color: #fff !important;
     }
   }
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
+  .login-particle {
+    height: 100%;
+    // position: absolute;
+  }
+  .inner-container {
+    width: 400px;
+    height: 500px;
+    position: absolute;
+    top: calc(50vh - 200px);
+    left: calc(50vw - 200px);
+    overflow: hidden;
+  }
+  .box {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    font-family: Helvetica;
+    color: #fff;
+    background: rgba(0, 0, 0, 0.13);
+    padding: 30px 0px;
+  }
+  .box h1 {
+    text-align: center;
+    margin: 30px 0;
+    font-size: 30px;
+    letter-spacing: 10px;
+  }
+  .box input:hover {
+    border: 1px solid rgba(46, 204, 113, 0.7);
+    transition: 0.5s;
+  }
+  .box input:focus,
+  .box input:active,
+  .box button:focus,
+  .box button:active {
+    outline: none;
+  }
+  .box button {
+    background: rgba(46, 204, 113, 0.7);
+    border: none;
+    border-radius: 10px;
+    color: #fff;
+    padding: 10px;
+    font-size: 20px;
+    width: 330px;
+    margin: 20px auto;
+    display: block;
+    cursor: pointer;
+    letter-spacing: 10px;
+  }
+  .box button:hover {
+    transform: scale(1.1, 1.1);
+    transition: 0.5s;
+  }
+  .box button:active {
+    background: #47cd88;
+  }
+  .box p {
+    font-size: 14px;
+    text-align: center;
+    letter-spacing: 5px;
+  }
+  .box p span {
+    cursor: pointer;
+    color: #666;
+  }
+  .box p span:hover {
+    color: rgba(46, 204, 113, 0.7);
+    transition: 0.5s;
   }
 }
-
-</style>
-
-<style rel="stylesheet/scss" lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
-.login-container {
-  position: fixed;
-  height: 100%;
+/* 小车 */
+.car-container {
+  padding-top: 50px;
+}
+*,
+:after,
+:before {
+  box-sizing: border-box;
+}
+.pull-left {
+  float: left;
+}
+.pull-right {
+  float: right;
+}
+.clearfix:after,
+.clearfix:before {
+  content: '';
+  display: table;
+}
+.clearfix:after {
+  clear: both;
+  display: block;
+}
+.car .mirror-wrap:before,
+.car .mirror-wrap:after,
+.car .mirror-inner:before,
+.car .mirror-inner:after,
+.car .middle .top:before,
+.car .middle .top:after,
+.car .lights:before,
+.car .lights:after,
+.car .bumper .top:before,
+.car .bumper .top:after,
+.car .bumper .middle:before,
+.car .tyres .tyre:before,
+.car .tyres .tyre:after {
+  content: '';
+  position: absolute;
+}
+.car {
+  z-index: 1;
+  margin: 0 auto;
+  position: relative;
+  display: inline-block;
+}
+.car .body {
+  z-index: 1;
+  position: relative;
+  animation: suspension 5s linear infinite;
+}
+.car .mirror-wrap {
+  width: 88px;
+  height: 30px;
+  margin: auto;
+  position: relative;
+  background-color: #fff;
+  border-top-left-radius: 45px 10px;
+  border-top-right-radius: 45px 10px;
+}
+.car .mirror-wrap:before,
+.car .mirror-wrap:after {
+  top: 8px;
+  border: 5px solid #1a1c20;
+  border-top: 15px solid transparent;
+}
+.car .mirror-wrap:before {
+  left: -5px;
+  border-left: 0 solid transparent;
+}
+.car .mirror-wrap:after {
+  right: -5px;
+  border-right: 0 solid transparent;
+}
+.car .mirror-inner {
+  top: 2px;
+  width: inherit;
+  height: inherit;
+  margin: inherit;
+  position: inherit;
+  background-color: #1a1c20;
+  border-top-left-radius: 50px 8px;
+  border-top-right-radius: 50px 8px;
+}
+.car .mirror-inner:before,
+.car .mirror-inner:after {
+  bottom: 0;
+  width: 10px;
+  height: 8px;
+  background-color: #252525;
+}
+.car .mirror-inner:before {
+  left: -12px;
+  border-radius: 2px 0 0 5px;
+}
+.car .mirror-inner:after {
+  right: -12px;
+  border-radius: 0 2px 5px 0;
+}
+.car .mirror {
   width: 100%;
-  background-color: $bg;
-  .login-form {
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 520px;
-    padding: 35px 35px 15px 35px;
-    margin: 120px auto;
+  z-index: 10;
+  height: 25px;
+  overflow: hidden;
+  position: relative;
+  border-top-left-radius: 45px 10px;
+  border-top-right-radius: 45px 10px;
+}
+.car .mirror .shine {
+  left: 50%;
+  width: 5px;
+  z-index: -1;
+  height: 40px;
+  position: absolute;
+  margin-left: -2.5px;
+  background-color: #3d3e3e;
+  animation: shine 5s linear infinite;
+}
+.car .middle {
+  z-index: 1;
+  margin-top: -2px;
+}
+.car .middle .top {
+  width: 98px;
+  height: 14px;
+  margin: auto;
+  position: relative;
+  background-color: #f7f7f7;
+}
+.car .middle .top:before,
+.car .middle .top:after {
+  top: 0;
+  border: 5px solid #f7f7f7;
+  border-top: 9px solid transparent;
+}
+.car .middle .top:before {
+  left: -7px;
+  border-left: 2px solid transparent;
+}
+.car .middle .top:after {
+  right: -7px;
+  border-right: 2px solid transparent;
+}
+.car .middle .top .line {
+  top: 2px;
+  height: 1px;
+  width: 44px;
+  margin: auto;
+  position: relative;
+  background-color: #bebebe;
+}
+.car .middle .bottom {
+  margin: auto;
+  width: 115px;
+  height: 20px;
+  margin-top: -2px;
+  background-color: #dfdfdf;
+  border-top-left-radius: 4px 5px;
+  border-top-right-radius: 4px 5px;
+}
+.car .lights {
+  top: 5px;
+  width: 111px;
+  height: 12px;
+  margin: auto;
+  position: relative;
+  border-radius: 2px;
+  background-color: #6a6a6a;
+}
+.car .lights:before,
+.car .lights:after {
+  top: 50%;
+  width: 16px;
+  height: 16px;
+  margin-top: -8px;
+  border-radius: 50%;
+  background-color: #fff;
+  border: 1px solid #777;
+}
+.car .lights:before {
+  left: 3px;
+}
+.car .lights:after {
+  right: 3px;
+}
+.car .lights .line {
+  top: 50%;
+  left: 50%;
+  width: 50%;
+  height: 1px;
+  background: #fff;
+  position: absolute;
+  transform: translateX(-50%);
+}
+.car .bumper .top {
+  width: 110px;
+  height: 10px;
+  margin: auto;
+  position: relative;
+  background-color: #202428;
+  border-radius: 0 0 6px 6px;
+  border-top: 1px solid #474949;
+  border-bottom: 1px solid #474949;
+}
+.car .bumper .top:before,
+.car .bumper .top:after {
+  top: 50%;
+  width: 10px;
+  height: 4px;
+  margin-top: -2px;
+  border-radius: 1px;
+  background-color: #fb8c00;
+}
+.car .bumper .top:before {
+  left: 5px;
+}
+.car .bumper .top:after {
+  right: 5px;
+}
+.car .bumper .middle {
+  height: 8px;
+  width: 102px;
+  margin: auto;
+  position: relative;
+  background-color: #cfcfcf;
+  border-radius: 0 0 6px 6px;
+}
+.car .bumper .middle:before {
+  top: 50%;
+  left: 50%;
+  color: #fff;
+  height: 12px;
+  font-size: 8px;
+  padding: 1px 4px;
+  font-weight: 500;
+  margin-top: -6px;
+  line-height: 10px;
+  text-align: center;
+  white-space: nowrap;
+  content: attr(data-numb);
+  background-color: #e9573f;
+  transform: translateX(-50%);
+}
+.car .bumper .bottom {
+  height: 6px;
+  width: 85px;
+  margin: auto;
+  position: relative;
+  background-color: #202428;
+  border-radius: 0 0 6px 6px;
+  box-shadow: 0 1px 11px rgba(0, 0, 0, 0.75);
+}
+.car .tyres {
+  margin: auto;
+  width: 110px;
+  position: relative;
+}
+.car .tyres .tyre {
+  width: 100%;
+  height: 40px;
+  bottom: -6.5px;
+  position: absolute;
+}
+.car .tyres .tyre:before {
+  left: -5px;
+  box-shadow: -2px 2px 0 #b7b7b8 inset;
+}
+.car .tyres .tyre:after {
+  right: -5px;
+  box-shadow: 2px 2px 0 #b7b7b8 inset;
+}
+.car .tyres .tyre:before,
+.car .tyres .tyre:after {
+  width: 18px;
+  height: 40px;
+  border-radius: 6px;
+  background-color: #1a1c20;
+  background: linear-gradient(to right, #333 50%, #555 50%);
+  background-size: 2px;
+}
+.car .tyres .tyre.back:before,
+.car .tyres .tyre.back:after {
+  bottom: 3px;
+}
+.car .tyres .tyre.back:before {
+  left: 12px;
+}
+.car .tyres .tyre.back:after {
+  right: 12px;
+}
+
+.road-wrap {
+  perspective: 160px;
+}
+.road-wrap .road {
+  margin-top: -360px;
+  transform: rotateX(80deg);
+}
+.road-wrap .lane-wrap {
+  animation: steer 5s linear infinite;
+}
+.road-wrap .lane {
+  width: 5px;
+  margin: auto;
+}
+.road-wrap .lane > div {
+  width: 100%;
+  margin: auto;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  background-color: #fff;
+  animation: lane 5s linear infinite;
+}
+.road-wrap .lane > div:nth-child(1) {
+  height: 15px;
+}
+.road-wrap .lane > div:nth-child(2) {
+  height: 20px;
+}
+.road-wrap .lane > div:nth-child(3) {
+  height: 30px;
+}
+.road-wrap .lane > div:nth-child(4) {
+  height: 50px;
+}
+.road-wrap .lane > div:nth-child(5) {
+  height: 40px;
+}
+.road-wrap .lane > div:nth-child(6) {
+  height: 50px;
+}
+.road-wrap .lane > div:nth-child(7) {
+  height: 40px;
+}
+.road-wrap .lane > div:nth-child(8) {
+  height: 50px;
+}
+.road-wrap .lane > div:nth-child(9) {
+  height: 30px;
+}
+.road-wrap .lane > div:nth-child(10) {
+  height: 20px;
+}
+.road-wrap .lane > div:nth-child(11) {
+  height: 15px;
+}
+
+@keyframes shine {
+  0%,
+  80%,
+  100% {
+    transform: translateX(-55px) rotate(24deg);
   }
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
+  5%,
+  15%,
+  25%,
+  35%,
+  45%,
+  55%,
+  65%,
+  75%,
+  85%,
+  95% {
+    background-color: #2d2d2d;
   }
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-    &_login {
-      font-size: 20px;
-    }
+  0%,
+  10%,
+  20%,
+  30%,
+  40%,
+  50%,
+  60%,
+  70%,
+  80%,
+  90%,
+  100% {
+    background-color: #4d4d4d;
   }
-  .title {
-    font-size: 26px;
-    font-weight: 400;
-    color: $light_gray;
-    margin: 0px auto 40px auto;
-    text-align: center;
-    font-weight: bold;
+  33%,
+  44% {
+    transform: translateX(30px) rotate(-14deg);
   }
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
+  66% {
+    transform: translateX(0px) rotate(-10deg);
+  }
+}
+@keyframes lane {
+  0% {
+    transform: translateY(320px);
+  }
+  100% {
+    transform: translateY(-160px);
+  }
+}
+@keyframes steer {
+  0%,
+  100% {
+    transform: translateX(-15px) rotate(5deg);
+  }
+  50% {
+    transform: translateX(15px) rotate(-5deg);
+  }
+}
+@keyframes suspension {
+  0%,
+  75%,
+  100% {
+    transform: rotate(3deg);
+  }
+  10%,
+  30%,
+  50%,
+  70%,
+  90% {
+    top: 0;
+  }
+  20%,
+  40%,
+  60%,
+  80%,
+  100% {
+    top: -1px;
+  }
+  25%,
+  50% {
+    transform: rotate(-3deg);
+  }
+  20% {
+    transform: rotate(0deg);
+  }
+  90% {
+    transform: rotate(-1deg);
   }
 }
 </style>
