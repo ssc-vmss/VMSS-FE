2ac619beba4f6a599a9ca40d7e2de34af0426cea
jiechina <pj.mail@foxmail.com>
2018-09-27 15:45:59 +0800

    API切换为服务器地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://10.213.204.193:8762"'
-  // BASE_API: '"http://10.213.204.60:18762"'
+  // BASE_API: '"http://10.213.204.193:8762"'
+  BASE_API: '"http://10.213.204.60:18762"'
 })
