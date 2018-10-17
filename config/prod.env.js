db058aa6acf5b800f04e79469c2f97a56201f6b2
jiechina <pj.mail@foxmail.com>
2018-10-17 14:21:49 +0800

    配置调整

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"http://10.213.204.60:18762"',
-  BASE_API: '"http://127.0.0.1:8762"',
+  BASE_API: '"http://119.6.241.135:8762"',
 }
